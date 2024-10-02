4063e6f713ac365419bb1d3ec141fe08119ea2d4
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-02 10:51:09 +0530

    doing

---
+++ BFS.java
@@ -0,0 +1,22 @@
+// Link: https://bit.ly/3bn84K8
+import java.util.*;
+
+public class BFS {
+    public static void main(String[] args) {
+        int v=5, e=4;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        adj.add(new ArrayList(Arrays.asList(1,2,3)));
+        adj.add(new ArrayList(Arrays.asList()));
+        adj.add(new ArrayList(Arrays.asList(4)));
+        adj.add(new ArrayList(Arrays.asList()));
+        adj.add(new ArrayList(Arrays.asList()));
+        
+        System.out.println(adj);
+
+        // ArrayList<Integer> ans = bfsOfGraph(v,adj);
+
+        
+    }
+    
+}
